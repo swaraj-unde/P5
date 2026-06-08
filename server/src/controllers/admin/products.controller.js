669005608f0e6cf6a2c4135a214abf4a067abfe2
Swaraj <swarajunde1@gmail.com>
2026-06-08 19:46:20 +0530

    prod control

---
+++ server/src/controllers/admin/products.controller.js
@@ -0,0 +1,24 @@
+import { imageUploadUtil } from "../../utils/cloudinary";
+
+const handleImageUpload = async (req, res) => {
+  try {
+    const b64 = Buffer.from(req.file.buffer).toString("base64");
+    const url = `data:${req.file.mimetype};base64,${b64}`;
+
+    const result = await imageUploadUtil(url);
+
+    return res.status(200).json({
+      success: true,
+      message: "Image Upload Successful",
+      result,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: "Error While Uploading The Image",
+      error: error.message,
+    });
+  }
+};
+
+export default handleImageUpload;
