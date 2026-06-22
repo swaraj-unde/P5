ed3f87cddaf1b58864827c385d2cee7286f1d313
Swaraj <swarajunde1@gmail.com>
2026-06-22 17:33:56 +0530

    address model

---
+++ server/src/models/address.model.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const AddressSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    address: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    city: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    pincode: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    phone: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    notes: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const Address = mongoose.model("Address", AddressSchema);
